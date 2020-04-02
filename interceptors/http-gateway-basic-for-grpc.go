6ca85d806c86936e3957f8d154807fdb855d37f2
zoenion <zoenion.services@gmail.com>
2020-04-02 15:40:12 +0200

    Added basic interceptor with authentication func

--- interceptors/http-gateway-basic-for-grpc.go
+++ interceptors/http-gateway-basic-for-grpc.go
@@ -44,3 +44,42 @@ func NewServerBasic(gatewaySecret, realm, username, password string) *basicAuthF
 		gatewaySecret: gatewaySecret,
 	}
 }
+
+type basicAuthForGRPCWithAuthenticationFunc struct {
+	realm, gatewaySecret string
+	credentialsProvider  func(string) string
+}
+
+func (sb *basicAuthForGRPCWithAuthenticationFunc) Handle(next http.HandlerFunc) http.HandlerFunc {
+	reqAuth := &xhttp.RequireAuth{
+		Realm: sb.realm,
+		Type:  "Basic",
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			xhttp.WriteResponse(w, http.StatusUnauthorized, reqAuth)
+			return
+		}
+
+		if password != sb.credentialsProvider(username) {
+			xhttp.WriteResponse(w, http.StatusUnauthorized, reqAuth)
+			return
+		}
+
+		userAuthorization := r.Header.Get("Authorization")
+		if userAuthorization != "" {
+			r.Header.Set("authorization", fmt.Sprintf("Gateway %s/%s", sb.gatewaySecret, userAuthorization))
+		}
+		next(w, r)
+	}
+}
+
+func NewBasicWithAuthenticationFunc(gatewaySecret, realm string, provider func(string) string) *basicAuthForGRPCWithAuthenticationFunc {
+	return &basicAuthForGRPCWithAuthenticationFunc{
+		realm:               realm,
+		gatewaySecret:       gatewaySecret,
+		credentialsProvider: provider,
+	}
+}
